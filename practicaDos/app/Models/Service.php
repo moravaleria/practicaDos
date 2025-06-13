fc78105e2f48e2836f24954a2ee32d909595a0f9
Valeria Mora Cardenas <vamorac@unal.edu.co>
2025-06-13 16:54:44 -0500

    MODEL Service

--- practicaDos/app/Models/Service.php
+++ practicaDos/app/Models/Service.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Appointment;
 
 class services extends Model
 {
+    public function appointment()
+    {
+        return $this->hasMany(Appointment::class);
+    }
     use HasFactory;
 }
