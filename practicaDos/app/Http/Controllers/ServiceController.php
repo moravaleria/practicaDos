3003d338184ff379eef9a459f742c3eaaef19406
Valeria Mora Cardenas <vamorac@unal.edu.co>
2025-06-13 16:31:16 -0500

    CRUD Service

--- practicaDos/app/Http/Controllers/ServiceController.php
+++ practicaDos/app/Http/Controllers/ServiceController.php
@@ -10,7 +10,8 @@ class ServiceController extends Controller
 
     public function index()
     {
-        
+        $services=Service::all();
+        return view('services.index',compact('services'));
     }
 
     
@@ -21,7 +22,13 @@ class ServiceController extends Controller
 
     public function store(Request $request)
     {
-        
+        $service=new Service();
+        $service->id=$request->id;
+        $service->name=$request->name;
+        $service->description=$request->description;
+        $service->prince=$request->prince;
+        $service->save();
+        return redirect()->route('services.index');
     }
 
     
@@ -33,18 +40,27 @@ class ServiceController extends Controller
     
     public function edit(Service $service)
     {
-        //
+        $service= Service::find($id);
+        return view ('services.edit',compact('service'));
     }
 
    
     public function update(Request $request, Service $service)
     {
-        
+        $service= Service::find($id);
+        $service->id=$request->id;
+        $service->name=$request->name;
+        $service->description=$request->description;
+        $service->prince=$request->prince;
+        $service->save();
+        return redirect()->route('services.index');
     }
 
     
     public function destroy(Service $service)
     {
-        
+        $service= Service::find($id);
+        $service->delete();
+        return redirect()->route('services.index');
     }
 }
