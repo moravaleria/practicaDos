ae6d5457414739f972c1a692b8ea028340e7367d
Valeria Mora Cardenas <vamorac@unal.edu.co>
2025-06-13 16:45:55 -0500

    MODEL Appointment

--- practicaDos/app/Models/Appointment.php
+++ practicaDos/app/Models/Appointment.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Car;
+use App\Models\Service;
 
 class appointments extends Model
 {
     use HasFactory;
+    public function car()
+    {
+        return $this->belongsTo(Car::class,'car_id');
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Sevice::class,'service_id');
+    }
 }
